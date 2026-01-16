40e1aebe8abd09662717aa7eb6a29a64f604e091
Himars142 <127928240+Himars142@users.noreply.github.com>
2026-01-16 19:58:32 +0200

    fix: computing gc content

---
+++ computing_gc_content.py
@@ -0,0 +1,31 @@
+filename = "rosalind_gc.txt"
+
+sequences = {}
+current_id = None
+
+with open(filename, 'r') as f:
+    for line in f:
+        line = line.strip()
+
+        if not line:
+            continue
+
+        if line.startswith(">"):
+            current_id = line[1:]
+            sequences[current_id] = ""
+        else:
+            sequences[current_id] += line
+
+biggest_cg_content = 0
+biggest_cg_content_id = None
+
+for sequence in sequences:
+    dna = sequences[sequence]
+    cg_count = dna.count("C") + dna.count("G")
+    current_cg_content = 100/len(dna) * cg_count
+    if(biggest_cg_content < current_cg_content):
+        biggest_cg_content = current_cg_content
+        biggest_cg_content_id = sequence
+
+print(biggest_cg_content_id)
+print(biggest_cg_content)
